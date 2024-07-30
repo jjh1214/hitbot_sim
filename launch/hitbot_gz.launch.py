0d479b658ed0484a922c697d9fbb67cbe6fb05b2
jjh1214 <scarlet777735@gmail.com>
2024-07-30 18:00:56 +0900

    Adjusting joint1 damping & TODO:Upload none gravity gz_world and add path

--- launch/hitbot_gz.launch.py
+++ launch/hitbot_gz.launch.py
@@ -1,129 +1,6 @@
-# from launch_ros.actions import Node
-# from launch import LaunchDescription
-# from launch.conditions import IfCondition
-# from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, IncludeLaunchDescription
-# from launch_ros.substitutions import FindPackageShare
-# from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
-# from launch.event_handlers import OnProcessExit
-# from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-# def generate_launch_description():
-
-#     urdf_path = PathJoinSubstitution(
-#         [FindPackageShare("hitbot_sim"), "urdf", "Z-Arm_10042C0_gazebo.urdf.xacro"]
-#     )
-
-#     robot_controllers = PathJoinSubstitution(
-#         [
-#             FindPackageShare("hitbot_sim"),
-#             "config",
-#             "hitbot_controller2.yaml",
-#         ]
-#     )    
-
-#     return LaunchDescription([
-#         DeclareLaunchArgument(
-#             name='urdf',
-#             default_value=urdf_path,
-#             description='URDF path'
-#         ),
-
-#         DeclareLaunchArgument(
-#             name='use_sim_time',
-#             default_value='true',
-#             description='Use simulation time'
-#         ),
-
-#         Node(
-#             package='robot_state_publisher',
-#             executable='robot_state_publisher',
-#             name='robot_state_publisher',
-#             output='screen',
-#             parameters=[
-#                 {
-#                     # 'use_sim_time': LaunchConfiguration('use_sim_time'),
-#                     'robot_description': Command(['xacro ', LaunchConfiguration('urdf')])
-#                 }
-#             ],
-#         ),
-
-#         Node(
-#             package="controller_manager",
-#             executable="ros2_control_node",
-#             name="controller_manager",
-#             parameters=[robot_controllers],
-#             output="both",
-#         ),
-
-#         IncludeLaunchDescription(
-#             PythonLaunchDescriptionSource(
-#                 [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
-#             ),
-#             launch_arguments={"gz_args": " -r -v 3 empty.sdf"}.items(),
-#         ),
-
-#         Node(
-#         package="ros_gz_sim",
-#         executable="create",
-#         output="screen",
-#         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
-#         # namespace=PathJoinSubstitution([LaunchConfiguration('name'), "gz"]),
-#         arguments=[
-#             "-topic",
-#             "robot_description",
-#             "-name",
-#             "Z_arm",
-#             "-allow_renaming",
-#             "true",
-#             "-x",
-#             "0",
-#             "-y",
-#             "0",
-#             "-z",
-#             "0",
-#             "-R",
-#             "0",
-#             "-P",
-#             "0",
-#             "-Y",
-#             "0",
-#         ],
-#     ),
-
-#         RegisterEventHandler(
-#             OnProcessExit(
-#                 target_action=Node(
-#                     package="controller_manager",
-#                     executable="ros2_control_node",
-#                     name="controller_manager",
-#                     parameters=[robot_controllers],
-#                     output="both",
-#                 ),
-                
-#                 on_exit=[
-#                     ExecuteProcess(
-#                         cmd=['ros2', 'control', 'load_controller','joint_state_broadcaster', '--set-state', 'active'],
-#                         output='screen'),
-                        
-#                     ExecuteProcess(
-#                         cmd=['ros2', 'control', 'load_controller','z_arm_moveit_controller', '--set-state', 'active'],
-#                         output='screen'),
-
-#                     ExecuteProcess(
-#                         cmd=['ros2', 'control', 'load_controller','z_arm_position_controller', '--set-state', 'active'],
-#                         output='screen'),
-
-#                     ExecuteProcess(
-#                         cmd=['ros2', 'control', 'load_controller','z_arm_joint_publisher', '--set-state', 'active'],
-#                         output='screen'),
-#                 ]
-#             )
-#         )
-#     ])
-
 from launch_ros.actions import Node
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, IncludeLaunchDescription, TimerAction
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch.event_handlers import OnProcessStart
@@ -213,19 +90,17 @@ def generate_launch_description():
             ],
         ),
 
-        RegisterEventHandler(
-            OnProcessStart(
-                target_action=controller_manager_node,
-                on_start=[
-                    ExecuteProcess(
-                        cmd=['ros2', 'control', 'load_controller', 'joint_state_broadcaster', '--set-state', 'active'],
-                        output='screen'
-                    ),
-                    ExecuteProcess(
-                        cmd=['ros2', 'control', 'load_controller', 'z_arm_position_controller', '--set-state', 'active'],
-                        output='screen'
-                    ),
-                ]
-            )
+        TimerAction(
+            period=5.0,
+            actions=[
+                ExecuteProcess(
+                    cmd=['ros2', 'control', 'load_controller', 'joint_state_broadcaster', '--set-state', 'active'],
+                    output='screen'
+                ),
+                ExecuteProcess(
+                    cmd=['ros2', 'control', 'load_controller', 'z_arm_position_controller', '--set-state', 'active'],
+                    output='screen'
+                ),
+            ]
         )
     ])
