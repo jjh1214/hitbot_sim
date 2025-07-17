7a4b56a20876aeb53ed55f7fab36d5289596d559
jjh1214 <scarlet777735@gmail.com>
2025-07-17 13:54:28 +0900

    Migration gz-classic -> gz-harmonic in jazzy

--- launch/hitbot_gz.launch.py
+++ launch/hitbot_gz.launch.py
@@ -1,106 +1,119 @@
-from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, IncludeLaunchDescription, TimerAction
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
-from launch.event_handlers import OnProcessStart
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    urdf_path = PathJoinSubstitution(
-        [FindPackageShare("hitbot_sim"), "urdf", "Z-Arm_10042C0_gazebo.urdf.xacro"]
-    )
+    def robot_state_publisher(context):
+        robot_description_content = Command(
+            [
+                PathJoinSubstitution([FindExecutable(name='xacro')]),
+                ' ',
+                PathJoinSubstitution([
+                    FindPackageShare('hitbot_sim'),
+                    'urdf',
+                    f'Z-Arm_10042C0_gazebo.urdf.xacro'
+                ]),
+            ]
+        )
+        robot_description = {'robot_description': robot_description_content}
+        node_robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[robot_description]
+        )
+        return [node_robot_state_publisher]
 
     robot_controllers = PathJoinSubstitution(
         [
-            FindPackageShare("hitbot_sim"),
-            "config",
-            "hitbot_controller2.yaml",
-        ]
-    )    
-
-    controller_manager_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        name="controller_manager",
-        parameters=[robot_controllers],
-        output="both",
+            FindPackageShare('hitbot_sim'),
+            'config',
+            'hitbot_controller2.yaml',
+        ],
     )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            name='urdf',
-            default_value=urdf_path,
-            description='URDF path'
-        ),
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-topic', 'robot_description', '-name',
+                   'hitbot', '-allow_renaming', 'true'],
+    )
 
-        DeclareLaunchArgument(
-            name='use_sim_time',
-            default_value='true',
-            description='Use simulation time'
-        ),
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster',
+                   '--param-file',
+                   robot_controllers,
+                   ],
+    )
 
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[
-                {
-                    'use_sim_time': LaunchConfiguration('use_sim_time'),
-                    'robot_description': Command(['xacro ', LaunchConfiguration('urdf')])
-                }
-            ],
-        ),
+    z_arm_position_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['z_arm_position_controller',
+                   '--param-file',
+                   robot_controllers,
+                   ],
+    )
 
-        controller_manager_node,
+    bridge_params = os.path.join(get_package_share_directory('hitbot_sim'),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+    )
 
+    ld = LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
-            ),
-            launch_arguments={"gz_args": " -r -v 3 empty.sdf"}.items(),
+                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
+                                       'launch',
+                                       'gz_sim.launch.py'])]),
+            launch_arguments=[('gz_args', [' -r -v 1 empty.sdf'])]),
+        
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
         ),
 
-        Node(
-            package="ros_gz_sim",
-            executable="create",
-            output="screen",
-            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
-            arguments=[
-                "-topic",
-                "robot_description",
-                "-name",
-                "Z_arm",
-                "-allow_renaming",
-                "true",
-                "-x",
-                "0",
-                "-y",
-                "0",
-                "-z",
-                "0",
-                "-R",
-                "0",
-                "-P",
-                "0",
-                "-Y",
-                "0",
-            ],
-        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[z_arm_position_controller_spawner],
+            )
+        ),        
 
-        TimerAction(
-            period=5.0,
-            actions=[
-                ExecuteProcess(
-                    cmd=['ros2', 'control', 'load_controller', 'joint_state_broadcaster', '--set-state', 'active'],
-                    output='screen'
-                ),
-                ExecuteProcess(
-                    cmd=['ros2', 'control', 'load_controller', 'z_arm_moveit_controller', '--set-state', 'active'],
-                    output='screen'
-                ),
-            ]
-        )
+        ros_gz_bridge,
+        gz_spawn_entity,
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='If true, use simulated clock'),
+        DeclareLaunchArgument(
+            'description_format',
+            default_value='urdf',
+            description='Robot description format to use, urdf or sdf'),
     ])
+    ld.add_action(OpaqueFunction(function=robot_state_publisher))
+    return ld
